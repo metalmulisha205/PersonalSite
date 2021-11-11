3c918d1b04cfd97183cbb022386537c940bda0ff
Cole McKee <cole6274@bellsouth.net>
2021-11-11 14:11:42 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+import datetime
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    name = "Cole"
+    currHour = datetime.datetime.now().hour
+    if currHour < 12:
+        welcome = "Good Morning, " + name + "!"
+    elif currHour < 18: 
+        welcome = "Good Afternoon, " + name + "!"
+    elif currHour < 21:
+        welcome = "Good Evening, " + name + "!"
+    else:
+        welcome = "Good Night, " + name + "!"
+    
+    return render_template('index.html', welcome=welcome)
+
+if __name__ == '__main__':
+    app.run(debug=False)
